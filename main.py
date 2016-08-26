6d4d90e6847f942604b8f951e7158822a267fe86
Minh-Long Do <minhlong94@yahoo.de>
2016-08-27 00:42:45 +0200

    Show exception content during webcrawler execution.

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ from crawlers.crawler import WebCrawler
 if __name__ == '__main__':
 	start_url = 'http://yoyowallet.com/'
 	web_crawler = WebCrawler(start_url=start_url)
-	web_crawler.crawl()
+	try:
+		web_crawler.crawl()
+		
+	except ValueError as err:
+		print(err)
