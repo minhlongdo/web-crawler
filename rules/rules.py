57053943935a6a1e01393e531dbf94e5eaf0fce8
Minh-Long Do <minhlong.langos@gmail.com>
2016-08-29 17:14:25 +0200

    Fix issue #1 regarding go through urls that are not in the same domain.

--- rules/rules.py
+++ rules/rules.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from urllib3.util import parse_url
+from tldextract import extract
 
 
 class CrawlSameDomainRule:
 	
 	@staticmethod
 	def same_domain(domain_url, url):
-		search_domain = parse_url(domain_url).hostname
-		return search_domain in url
+		search_domain = extract(domain_url).domain
+		url_domain = extract(url).domain
+		return search_domain == url_domain
