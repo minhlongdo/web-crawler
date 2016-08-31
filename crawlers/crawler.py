2ac398b85ea6f2535e56657d9e903371aff1fffb
Minh-Long Do <minhlong.langos@gmail.com>
2016-08-31 18:37:04 +0200

    Implemented multithreaded webcrawler. Now need to test and see if it actually works.

--- crawlers/crawler.py
+++ crawlers/crawler.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
-from threading import Thread
-from queue import LifoQueue, Queue
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from queue import Queue, LifoQueue
 from parsers.parsers import PageParser
 from rules.rules import DomainRule, FileExtensionRule
 from handlers.http_handler import HttpHandler
@@ -23,12 +23,12 @@ ch.setFormatter(formatter)
 module_logger.addHandler(fh)
 module_logger.addHandler(ch)
 
-    
+
 class WebCrawler:
     def __init__(self, start_url=None):
         self.start_url = start_url
 
-    def start_crawl(self, num_thread=1):
+    def start_crawl(self, workers=1):
         """
         Start webcrawler workers using threads
         :param num_thread: Number of workers to run
@@ -37,19 +37,127 @@ class WebCrawler:
         site_map = {}
         visited = set()
         links_with_issues = set()
-        queue = Queue()
+        queue = set()
+
+        queue.add(self.start_url)
 
         if not self.start_url:
             raise ValueError("Invalid starting url value, starting_url=%s" % self.start_url)
 
-        queue.put(self.start_url)
+        if workers < 1:
+            raise ValueError("The number of workers need to be at least 1.")
 
-        for x in range(num_thread):
-            crawler_worker = WebCrawlerWorker(self.start_url, queue, visited, site_map, links_with_issues)
-            crawler_worker.daemon = True
-            crawler_worker.start()
+        jobs = 0
 
-        queue.join()
+        with ThreadPoolExecutor(max_workers=workers) as executor:
+            while len(queue) > 0 or jobs > 0:
+                future_to_url = dict((executor.submit(self.crawl_worker, url), url) for url in queue)
+                jobs += len(future_to_url)
+                queue.clear()
+
+                for future in as_completed(future_to_url):
+                    url = future_to_url[future]
+
+                    #queue.remove(url)
+                    visited.add(url)
+
+                    jobs -= 1
+
+                    if future.exception() is not None:
+                        module_logger.warn('%r generated an exception: %s' % (url,
+                                                                              future.exception()))
+                    else:
+                        start_url, site_map_entry, links_with_issues_entry = future.result()
+                        module_logger.info("Visited=%s linksWithIssues=%s Sitemap=%s" % (visited, links_with_issues, site_map))
+
+                        if site_map_entry is not None:
+                            site_map.update(site_map_entry)
+
+                            try:
+                                links = site_map_entry[url]['links']
+
+                                next_url_batch = [link for link in links
+                                                  if link is not None
+                                                  and link not in visited
+                                                  and DomainRule.apply(self.start_url, link)
+                                                  and not FileExtensionRule.apply(link)]
+
+                                for next_url in next_url_batch:
+                                    queue.add(next_url)
+
+                            except ValueError as err:
+                                module_logger.warn(err)
+
+                            except KeyError as err:
+                                module_logger.warn(err)
+
+                            except Exception as err:
+                                module_logger.warn(err)
+
+                        if links_with_issues_entry is not None:
+                            for link in links_with_issues_entry:
+                                links_with_issues.add(link)
+
+        return self.start_url, site_map, links_with_issues
+
+    def crawl_worker(self, url):
+        if url is None:
+            raise ValueError("Url=%s has a None value" % url)
+
+        site_map_entry = dict()
+        links_with_issues_entry = set()
+
+        module_logger.info("Working on url=%s" % url)
+
+        try:
+            access_link = LinkHandler.reconstruct_link(self.start_url, url)
+
+            if access_link is None:
+                raise ValueError("Access link value: %s" % access_link)
+
+            elif not DomainRule.apply(self.start_url, access_link):
+                module_logger.info("url=%s is not in the same domain as %s" % (access_link, self.start_url))
+                module_logger.debug("Start url=%s site_map_entry=%s links_with_issues_entry=%s" % (self.start_url,
+                                                                                                   site_map_entry,
+                                                                                                   links_with_issues_entry))
+                site_map_entry[url] = {'links': set(), 'assets': set()}
+                return self.start_url, site_map_entry, links_with_issues_entry
+
+            module_logger.debug("Going to open access_link=%s" % access_link)
+
+        except Exception as err:
+            module_logger.warn(err)
+            site_map_entry[url] = {'links': set(), 'assets': set()}
+            return self.start_url, site_map_entry, links_with_issues_entry
+
+
+        try:
+            content = HttpHandler.fetch_url_content(access_link)
+
+            if content is None:
+                raise ValueError("Content of the url=%s is None" % url)
+
+        except ValueError as err:
+            module_logger.warn(err)
+            site_map_entry[url] = {'links': set(), 'assets': set()}
+            return self.start_url, site_map_entry, links_with_issues_entry
+
+        except Exception as err:
+            module_logger.warn(err)
+            site_map_entry[url] = {'links': set(), 'assets': set()}
+            return self.start_url, site_map_entry, links_with_issues_entry
+
+        links, assets = PageParser.parse_page_get_links(content)
+
+        site_map_entry[url] = {'links': links, 'assets': assets}
+
+        module_logger.info("Completed working on url=%s" % url)
+
+        module_logger.info("SiteMap=%s" % site_map_entry)
+        module_logger.info("Links with issues=%s" % links)
+
+        return self.start_url, site_map_entry, links_with_issues_entry
+
 
     def crawl(self, start_url=None):
         """
@@ -59,22 +167,19 @@ class WebCrawler:
         """
         if self.start_url is None and start_url is None:
             raise ValueError("Start url cannot be None")
-        
+
         if start_url is not None:
             self.start_url = start_url
-        
+
         site_map = dict()
         visited = set()
         links_with_issues = set()
         queue = LifoQueue()
         queue.put(self.start_url)
-        
-        print("Crawling start")
-        
+
         while not queue.empty():
-            print("Current queue size=%i" % queue.qsize())
             next_link = queue.get()
-            
+
             module_logger.info("Retrieved url=%s from queue" % next_link)
 
             try:
@@ -104,158 +209,57 @@ class WebCrawler:
 
                 else:
                     module_logger.info("Going to access url=%s constructed from %s" % (access_link, next_link))
-                    
+
             except ValueError as err:
                 module_logger.warn(err)
                 links_with_issues.add(next_link)
                 continue
-            
+
             except Exception as err:
                 module_logger.error("An unexpected error during the link construction of url=%s" % next_link, err)
                 links_with_issues.add(next_link)
                 continue
-            
+
             try:
                 content = HttpHandler.fetch_url_content(access_link)
 
                 if content is None:
                     module_logger.warn("Unable to get content from link=%s" % access_link)
                     continue
-            
+
             except ValueError as err:
                 module_logger.warn("Link=%s has a value issue, value current is %s" % (access_link, content), err)
                 module_logger.exception(err)
                 continue
-            
+
             except Exception as err:
                 module_logger.warn("Something unexpected happened while fetching content of the url=%s"
                                    % access_link)
                 module_logger.exception(err)
                 continue
-            
+
             links, assets = PageParser.parse_page_get_links(content)
-            
+
             module_logger.debug("Add link=%s into already visited list" % next_link)
-            
+
             visited.add(next_link)
-            
+
             module_logger.info("Extracted from url=%s - links=%s assets=%s" % (access_link, links, assets))
-            
+
             for link in links:
                 if link not in visited:
                     queue.put(link)
-                
+
             module_logger.debug("Current link queue=%s" % str(queue))
             site_map_record = {next_link: {'links': links, 'assets': assets}}
-            
+
             module_logger.info("Adding record into site map=%s" % site_map_record)
-            
+
             site_map.update(site_map_record)
-        
+
         module_logger.info("Crawling completed.")
-        print("Crawling completed")
-        
+
         module_logger.info("SiteMap=%s" % site_map)
         module_logger.info("Links with issues=%s" % links_with_issues)
-        
+
         return self.start_url, site_map, links_with_issues
-
-
-class WebCrawlerWorker(Thread):
-    def __init__(self, start_url, visited, queue, site_map, links_with_issues):
-        super().__init__()
-        self.start_url = start_url
-        self.visited = visited
-        self.queue = queue
-        self.site_map = site_map
-        self.link_with_issues = links_with_issues
-
-    def run(self):
-        while True:
-            print("Current queue size=%i" % self.queue.qsize())
-            next_link = self.queue.get()
-
-            module_logger.info("Retrieved url=%s from queue" % next_link)
-
-            try:
-                if FileExtensionRule.apply(next_link):
-                    module_logger.info("Url=%s is a file asset" % next_link)
-                    continue
-
-            except ValueError as err:
-                module_logger.warn(err)
-
-            try:
-                access_link = LinkHandler.reconstruct_link(self.start_url, next_link)
-
-                if access_link is None:
-                    module_logger.warn("Currently working on next_link=%s - But access link value is None,"
-                                       "Something went wrong during the link construction" % next_link)
-                    self.link_with_issues.add(next_link)
-                    continue
-
-                elif access_link in self.visited:
-                    module_logger.info("Already visited url=%s, skipping" % access_link)
-                    continue
-
-                elif not DomainRule.apply(self.start_url, access_link):
-                    module_logger.info("url=%s is not in the same domain as %s" % (access_link, self.start_url))
-                    continue
-
-                else:
-                    module_logger.info("Going to access url=%s constructed from %s" % (access_link, next_link))
-
-            except ValueError as err:
-                module_logger.warn(err)
-                self.link_with_issues.add(next_link)
-                continue
-
-            except Exception as err:
-                module_logger.error("An unexpected error during the link construction of url=%s" % next_link, err)
-                self.link_with_issues.add(next_link)
-                continue
-
-            try:
-                content = HttpHandler.fetch_url_content(access_link)
-
-                if content is None:
-                    module_logger.warn("Unable to get content from link=%s" % access_link)
-                    continue
-
-            except ValueError as err:
-                module_logger.warn("Link=%s has a value issue" % access_link)
-                module_logger.exception(err)
-                continue
-
-            except Exception as err:
-                module_logger.warn("Something unexpected happened while fetching content of the url=%s"
-                                   % access_link)
-                module_logger.exception(err)
-                continue
-
-            links, assets = PageParser.parse_page_get_links(content)
-
-            module_logger.debug("Add link=%s into already visited list" % next_link)
-
-            self.visited.add(next_link)
-
-            module_logger.info("Extracted from url=%s - links=%s assets=%s" % (access_link, links, assets))
-
-            for link in links:
-                if link not in self.visited:
-                    self.queue.put(link)
-
-            module_logger.debug("Current link queue=%s" % str(self.queue))
-            site_map_record = {next_link: {'links': links, 'assets': assets}}
-
-            module_logger.info("Adding record into site map=%s" % site_map_record)
-
-            self.site_map.update(site_map_record)
-
-        module_logger.info("Crawling completed.")
-        print("Crawling completed")
-
-        module_logger.info("SiteMap=%s" % self.site_map)
-        module_logger.info("Links with issues=%s" % self.link_with_issues)
-
-        self.queue.task_done()
--- main.py
+++ main.py
@@ -2,18 +2,92 @@
 from crawlers.crawler import WebCrawler
 import pprint
 
+import time
+
+def single_threaded_web_crawler():
+	start = time.time()
+
+	try:
+		start_url, site_map, links_with_issues = web_crawler.crawl('http://yoyowallet.com/')
+	except ValueError as err:
+		print(err)
+
+	end = time.time()
+	print("Single threaded web crawler total keys: %i" % len(site_map.keys()))
+	print("single threaded web crawler total time: %d" % (end - start))
+
+	return site_map
+
+def single_thread_pool_web_crawler():
+	start = time.time()
+
+	try:
+		start_url, site_map, links_with_issues = web_crawler.start_crawl(workers=1)
+	except ValueError as err:
+		print(err)
+
+	end = time.time()
+	print("Single threaded pool web crawler total keys: %i" % len(site_map.keys()))
+	print("Single thread pool worker total time: %d" % (end - start))
+
+	return site_map
+
+
+def two_threaded_pool_web_crawler():
+	start = time.time()
+
+	try:
+		start_url, site_map, links_with_issues = web_crawler.start_crawl(workers=2)
+	except ValueError as err:
+		print(err)
+
+	end = time.time()
+	print("Two threaded web crawler total keys: %i" % len(site_map.keys()))
+	print("Two thread pool worker total time: %d" % (end - start))
+
+	return site_map
+
+def n_thread_pool_web_Crawler(n):
+	start = time.time()
+
+	try:
+		start_url, site_map, links_with_issues = web_crawler.start_crawl(workers=n)
+	except ValueError as err:
+		print(err)
+
+	end = time.time()
+	print("%i threaded web crawler total keys: %i" % (n, len(site_map.keys())))
+	print("%i thread pool worker total time: %d" % (n, end - start))
+
+	return site_map
+
+def compare_keys(result1, result2):
+	if len(result1.keys()) == len(result2.keys()):
+		print("Number of keys are the same")
+	else:
+		print("Different number of keys")
 
 if __name__ == '__main__':
 	start_url = 'http://yoyowallet.com/'
 	web_crawler = WebCrawler(start_url=start_url)
 	pp = pprint.PrettyPrinter(indent=4)
-	
-	try:
-		start_url, site_map, links_with_issues = web_crawler.crawl()
-		
-		pp.pprint(start_url)
-		pp.pprint(site_map)
-		pp.pprint(links_with_issues)
-		
-	except ValueError as err:
-		print(err)
+
+	#single_threaded_result = single_threaded_web_crawler()
+	single_thread_pool_result = single_thread_pool_web_crawler()
+	#two_threaded_pool_result = two_threaded_pool_web_crawler()
+	#ten_threaded_pool_result = n_thread_pool_web_Crawler(10)
+	#twenty_threaded_pool_result = n_thread_pool_web_Crawler(20)
+
+	#print(len(single_threaded_result))
+	#print(len(single_thread_pool_result))
+	#print(len(two_threaded_pool_result))
+	#print(len(ten_threaded_pool_result))
+	#print(len(twenty_threaded_pool_result))
+
+	#pp.pprint(single_threaded_result)
+	pp.pprint(single_thread_pool_result)
+	print(len(single_thread_pool_result))
+	#pp.pprint(two_threaded_pool_result)
+	#pp.pprint(ten_threaded_pool_result)
+	#pp.pprint(twenty_threaded_pool_result)
+
